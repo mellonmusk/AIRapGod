1f94cd4c21c401aff131bc306c90a052b5208c9b
hj kim <hojeongkim24@gmail.com>
2024-12-12 21:13:37 +0900

    commit

--- main_copy.py
+++ main_copy.py
@@ -12,25 +12,22 @@ if not api_token:
     st.info("Please add your HuggingFace API key to continue.")
     st.stop()
 image_client = InferenceClient("ZB-Tech/Text-to-Image")
-if 'reference_song' not in st.session_state:
-    st.session_state.reference_song = "Default"
     
 with st.sidebar:
     reference_song = st.radio( 
     "Select a song style to reference and listen below.🎧 You can also download it!",
     ["Default", "Lose Yourself(2002)", "Monster(2013)", "Fuel(2024)" ],
     captions=[
-        "This bot studied all of Eminem's albums and lyrics, so the bot will choose for you. You can hear his voice at an interview below."
+        "This bot studied all of Eminem's albums and lyrics, so the bot will choose for you. You can hear his voice at an interview below.",
         "With a steady, driving beat and intense lyrical delivery, this song captures the feeling of urgency and determination, reflecting Eminem’s determination to seize the moment.",
         "A smoother, more radio-friendly flow with introspective yet accessible lyrics, differentiated from Eminem's other songs by its pop-heavy structure and Rihanna's anthemic chorus, which softens the typically aggressive tone of his verses.",
         "Featuring a fast-paced, aggressive flow, this track unleashes a wave of energy and anger, with a raw, intense delivery that reflects Eminem’s power and urgency.",
     ],
-    key = "reference_song"
 )
 
     # Map radio button options to their respective audio files
     audio_files = {
-    "Lose Yourself(2002)": "Lose Yourself.m4a",
+    "Lose Yourself(2002)": "LoseYourself.m4a",
     "Monster(2013)": "Monster.m4a",
     "Fuel(2024)": "Fuel.m4a", 
     "Default": "eminemVoice.m4a",
@@ -114,7 +111,7 @@ Given the topic, generate a rap verse inspired by it.\
 \
 Now, answer the questions and provide the rap verse, the song references, and the rhymes considered."
     else:
-        content = "You are AI Eminem. Eminem, a.k.a rap god, is the greatest rapper of all time and he is good at lyrical miracle. Talk like him. Also, I will give you a topic so create rap verses about it as if you are Eminem. Study all his previous albums and lyrics, and then create verses just like him. I want Eminem Style verses. Your name is AI Eminem, a.k.a AI rapgod. Given the topic, generate a rap verse inspired by it.1. Provide a list of words that rhyme with the word f'{topic}' (including both exact and near rhymes). Do the same thing using one word related to f'{topic}' as keyword. 2. Create a rap verse that incorporates the theme of f'{topic}' using all the words in rhyme lists you've created.3. Which of Eminem's songs did you draw inspiration from while creating this verse?4. What rhymes or lyrical techniques did you consider to craft the verse?Now, answer the questions and provide the rap verse, the song references, and the rhymes considered."
+        content = "You are AI Eminem. Eminem, a.k.a rap god, is the greatest rapper of all time and he is good at lyrical miracle. Talk like him. Also, I will give you a topic so create rap verses about it as if you are Eminem. Study all his previous albums and lyrics, and then create verses just like him. I want Eminem Style verses. Your name is AI Eminem, a.k.a AI rapgod. Given the topic, generate a rap verse inspired by it. 1. Provide a list of words that rhyme with the word '{topic}' (including both exact and near rhymes). Do the same thing using one word related to '{topic}' as keyword. 2. Create a rap verse that incorporates the theme of '{topic}' using all the words in rhyme lists you've created.3. Which of Eminem's songs did you draw inspiration from while creating this verse? 4. What rhymes or lyrical techniques did you consider to craft the verse? Now, answer the questions and provide the rap verse, the song references, and the rhymes considered."
 
     st.session_state.messages.append({"role": "user", "content": content})
     st.session_state.messages.append({"role": "user", "content": f"The topic is '{topic}'."})
